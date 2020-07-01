e309c5e9a7867a139173d46f7e7468fce98696e2
jairo <jairo.rotava@gmail.com>
2020-07-01 14:30:28 -0300

    fix ast converter

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
       license='MIT',
       author='jairo',
       author_email='jairo.rotava@gmail.com',
-      install_requires=['netCDF4', 'pandas', 'numpy', 'python-dateutil', 'cfchecker'],
+      install_requires=['netCDF4', 'pandas', 'numpy', 'python-dateutil', 'cfchecker', 'cfunits'],
       tests_requires=['pytest'],
       cmdclass={'test': PyTest},
       long_description='descrcicao longa',

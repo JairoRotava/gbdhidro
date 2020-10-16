e97f97bc883bf76ff91c98e777d1edaafca3ae41
jairo <jairo.rotava@gmail.com>
2020-10-16 18:21:02 -0300

    modificacao do find

--- gbdhidro/database/find.py
+++ gbdhidro/database/find.py
@@ -24,6 +24,7 @@ from pymongo import MongoClient
 from gbdhidro.netcdf import cf
 import numpy
 import datetime
+import iso8601
 
 DEBUG = False
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -33,6 +34,40 @@ LOG_FILE = 'upload.log'
 MONGODB_URL = '127.0.0.1:27017'
 DATABASE = 'gbdhidro'
 COLLECTION = 'index'
+
+import argparse
+import os
+import glob
+import subprocess
+import logging
+import sys
+import os
+from netCDF4 import Dataset
+from shutil import copyfile
+import zipfile
+from pymongo import MongoClient
+from gbdhidro.netcdf import cf
+import numpy
+
+DEBUG = False
+HERE = os.path.abspath(os.path.dirname(__file__))
+INPUT_FOLDER = os.path.realpath('./test/output/hobo_ua_003_64')
+OUTPUT_FOLDER = os.path.realpath('../test/output/load_netcdf_to_db/database_root')
+CONVERTER_LIST = [os.path.join(HERE, '../station_raw_to_netcdf/hobo_ua_003_64/hobo_ua_003_64_to_netcdf.py')]
+FILE_OVERWRITE = True
+ERROR_CODE = 1
+LOG_FILE = 'upload.log'
+# Mongodb info
+MONGODB_URL = '127.0.0.1:27017'
+DATABASE = 'gbdhidro'
+COLLECTION = 'index'
+
+DATABASE_DEFAULT_PATH = os.path.join(os.path.expanduser('~'), 'gbdroot')
+
+# Inicia logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 
 
 # para encontrar
@@ -79,12 +114,24 @@ query = {
     '$and': query_list
 }
 
-def find(uuid=None, date=None, latlon=None, keywords=None):
+def make_query(uuid=None, before=None, after=None, latlon=None, keywords=None, custom=None):
     query_list = []
     # uuid
     if uuid:
         query_uuid = {'database_uuid': {'$regex': uuid, '$options': 'i'}}
         query_list.append(query_uuid)
+
+    # check for data before date
+    if before:
+        query_list.append({'time_coverage_start': {'$lte': before}})
+
+    # check for data after date
+    if after:
+        query_list.append({'time_coverage_end': {'$gte': after}})
+
+
+
+    date = None
     # start/stop dates
     if date:
         # Query para pegar qualquer dado que tenha algum overlap de tempo nessa faixa
@@ -118,23 +165,88 @@ def find(uuid=None, date=None, latlon=None, keywords=None):
         query_keywords = {'keywords': {'$in': keywords}}
         query_list.append(query_keywords)
 
+    if custom:
+        query_custom = {}
+        query_list.append(query_custom)
+
     # Finaliza query composta
-    query = {
-        '$and': query_list
-    }
-    return query
+    if len(query_list) >= 1:
+        p_query = {
+            '$and': query_list
+        }
+    else:
+        p_query = {}
+
+    return p_query
 
 
-client = MongoClient(MONGODB_URL)
-gbd = client[DATABASE]
-index = gbd[COLLECTION]
+def command_line():
+    """
+    Captura dados de linhas de comando
+    """
+    TOOL_DESCRIPTION = 'Find item in database'
+    parser = argparse.ArgumentParser(description=TOOL_DESCRIPTION)
+    parser.add_argument('-uuid', type=str, help="uuid")
+    parser.add_argument('-b', "--before", type=str, help="before datetime ISO8601 (eg. 2004-06-23T22:00:00Z)")
+    parser.add_argument('-a', "--after", type=str, help="after datetime ISO8601 (eg. 2004-06-23T22:00:00Z)")
+    parser.add_argument('--north_of', type=str, help="north of latitude (decimal)")
+    parser.add_argument('--south_of', type=str, help="south of latitude (decimal)")
+    parser.add_argument('--east_of', type=str, help="east of longitude (decimal)")
+    parser.add_argument('--west_of', type=str, help="west of longitude (decimal)")
+    parser.add_argument('-r', "--region",  type=str, help="region")
+    parser.add_argument('-k', '--keywords', type=str, help="keywords: 'keyword1, keyword2'")
+    parser.add_argument('-c', '--custom', type=str, help="custom search:  '(mongodb syntax)'")
+    args = parser.parse_args()
 
-mydoc = index.find(find(date=[start_date, end_date]))
-for x in mydoc:
-    print(x)
+    uuid = args.uuid
+    #period = args.period
+    region = args.region
 
-#client.close()
+    #    Date and time in UTC
+    #    2020-10-16T19:27:42+00:00
+    #    2020-10-16T19:27:42Z
+    #    20201016T192742Z
+    if args.before:
+        before = iso8601.parse_date(args.before)
+        logger.debug('Before: {}'.format(before))
+    else:
+        before = None
 
+    if args.after:
+        after = iso8601.parse_date(args.after)
+        logger.debug('After: {}'.format(after))
+    else:
+        after = None
+
+    if args.keywords:
+        keywords = args.keywords.split(',')
+        keywords = [k.strip() for k in keywords]
+    else:
+        keywords = None
+    custom = args.custom
+
+    client = MongoClient(MONGODB_URL)
+    gbd = client[DATABASE]
+    index = gbd[COLLECTION]
+    my_query = make_query(uuid=uuid, before=before, after=after, latlon=region, keywords=keywords, custom=custom)
+    logger.debug('Mongodb query: {}'.format(my_query))
+    items = index.find(my_query)
+    total_found = 0
+    for i in items:
+        print(i)
+        total_found += 1
+
+    print('Found {} items'.format(total_found))
+
+    client.close()
+
+
+# Chamado da linha de comando
+if __name__ == "__main__":
+    if DEBUG:
+        logger.setLevel(level=logging.DEBUG)
+    else:
+        command_line()
 
 
 
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
       license='MIT',
       author='jairo',
       author_email='jairo.rotava@gmail.com',
-      install_requires=['netCDF4', 'pandas', 'numpy', 'python-dateutil', 'cfchecker', 'cfunits', 'pymongo'],
+      install_requires=['netCDF4', 'pandas', 'numpy', 'python-dateutil', 'cfchecker', 'cfunits', 'pymongo', 'iso8601'],
       tests_requires=['pytest'],
       cmdclass={'test': PyTest},
       long_description='descricao longa',

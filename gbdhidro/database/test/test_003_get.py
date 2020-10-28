053023a9d477bf99f7f8e0aa616c5a7757dbda17
jairo <jairo.rotava@gmail.com>
2020-10-28 14:47:42 -0300

    Fix create output folder

--- gbdhidro/database/test/test_003_get.py
+++ gbdhidro/database/test/test_003_get.py
@@ -6,11 +6,10 @@ import db_test_credentials as cr
 
 here = os.path.abspath(os.path.dirname(__file__))
 UUID = 'gbdhidro/estacoes/eh-p02/eh-p02_20191212t150000z_20200226t200800z.nc'
-DST_FOLDER = os.path.realpath(os.path.join(here, './output/get_netcdf'))
+DST_FOLDER = os.path.realpath(os.path.join(here, './output/get'))
+os.makedirs(DST_FOLDER, exist_ok=True)
 EXEC_PATH = os.path.realpath(os.path.join(here, '../get.py'))
 USER = '{}:{}@{}:{}/{}'.format(cr.SFTP_USER, cr.SFTP_PASSWORD, cr.SFTP_HOSTNAME, cr.SFTP_PORT, cr.SFTP_ROOT)
-#PASSWORD = cr.SFTP_PASSWORD
-#ROOT = cr.SFTP_ROOT
 
 
 def test_get_netcdf_to_db():
